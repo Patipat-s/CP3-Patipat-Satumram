5ceccd979ccf5918d57fa27aff05af6979061a36
Patipat-s <71583852+Patipat-s@users.noreply.github.com>
2020-09-23 01:42:36 +0700

    Delete Exercise4_Patipat_S.py
    Wrong Name

--- Exercise4_Patipat_S.py
+++
@@ -1,9 +0,0 @@
-FEscore = 80
-GBscore = 85
-ICSscore = 75
-CPscore = 90
-print("--------Your Score--------")
-print("Foundation English :              ", FEscore)
-print("General Business :                ", GBscore)
-print("Introduction to Computer Systems :", ICSscore)
-print("Computer Programming :            ", CPscore)
