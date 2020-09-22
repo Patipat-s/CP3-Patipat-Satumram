6ce540a041298f4416ffdfcb5ce2a4597ed2f38a
Patipat-s <dzombie2540@gmail.com>
2020-09-23 01:40:07 +0700

    Variable Test
    EX4 Variable Example Test

---
+++ Exercise4_Patipat_S.py
@@ -0,0 +1,9 @@
+FEscore = 80
+GBscore = 85
+ICSscore = 75
+CPscore = 90
+print("--------Your Score--------")
+print("Foundation English :              ", FEscore)
+print("General Business :                ", GBscore)
+print("Introduction to Computer Systems :", ICSscore)
+print("Computer Programming :            ", CPscore)
